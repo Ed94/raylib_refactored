1a8048fb3a953d16591936eb0c1b326b96dc808a
Ed_ <edwardgz@gmail.com>
2023-11-17 19:48:41 -0500

    Refactor and build script complete. (Win, msvc)

--- projects/scripts/refactor.ps1
+++ projects/scripts/refactor.ps1
@@ -1,22 +1,35 @@
-$raylib_headers  = Get-ChildItem -Path $path_raylib_src       -Filter '*.h' -File
-$raylib_modules  = get-childitem -path $path_raylib_src       -filter '*.c' -file
+$path_root     = git rev-parse --show-toplevel
+$path_scripts  = $PSScriptRoot
+
+$path_raylib_src         = join-path $path_root       'src'
+$path_raylib_platforms   = join-path $path_raylib_src 'platforms'
+$path_raylib_glfw_inc    = join-path $path_raylib_src 'external/glfw/include'
+$path_raylib_gputex      = join-path $path_raylib_src 'external/rl_gputex.h'
+$path_refactor           = join-path $PSScriptRoot    'raylib_c.refactor'
+$path_refactor_rlgl      = join-path $PSScriptRoot    'raylib_c_gl.refactor'
+
+$raylib_headers  = Get-ChildItem -Path $path_raylib_src -Filter '*.h' -File
+$raylib_modules  = get-childitem -path $path_raylib_src -filter '*.c' -file
 
 # Refactor with refactor.exe
 if ( $true ) {
-	$path_refactor      = join-path $path_raylib 'raylib_c.refactor'
-	$path_refactor_rlgl = join-path $path_raylib 'raylib_c_gl.refactor'
-
-	$fmt_includes = @()
+	$files = @()
 	foreach ( $header in $raylib_headers ) {
-		$fmt_includes +=  split-path $header -leaf
+		$file_name = split-path $header -leaf
+
+		if ( -not $file_name.Equals('rlgl.h' ) ) {
+			$files += "$header"
+		}
 	}
 	foreach ( $module in $raylib_modules ) {
-		$fmt_includes +=  split-path $module -leaf
+		$files += "$module"
 	}
+
+	$files += "$path_raylib_gputex"
 
 	$platform_modules = @()
 	foreach ( $module in (get-childitem -path $path_raylib_platforms -filter '*.c' -file) ) {
-		$platform_modules += split-path $module -leaf
+		$platform_modules += "$module"
 	}
 
 	$path_rlgl = join-path $path_raylib_src 'rlgl.h'
@@ -26,8 +39,8 @@ if ( $true ) {
 		$refactors = @(@())
 		$refactorParams = @(
 			# "-debug",
-			"-num=$($fmt_includes.Count)"
-			"-src=$($fmt_includes)",
+			"-num=$($files.Count)"
+			"-src=$($files)",
 			"-spec=$($path_refactor)"
 		)
 		& refactor $refactorParams
@@ -48,7 +61,7 @@ if ( $true ) {
 	Pop-Location
 
 	Push-Location $path_raylib_src
-		$gl_modules = @( "$path_rlgl" )
+		$gl_modules = @( "$path_rlgl", "$path_raylib_gputex" )
 
 		write-host "Beginning refactor just for rlgl.h...`n"
 		$refactors = @(@())
